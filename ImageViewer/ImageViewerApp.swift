99b44676ad924280058205fac7540a73bf08c6b5
Phumrapee Limpianchop <git@rayriffy.com>
2025-05-06 04:42:20 +0700

    Initial Commit

---
+++ ImageViewer/ImageViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageViewerApp.swift
+//  ImageViewer
+//
+//  Created by Phumrapee Limpianchop on 2025/05/06.
+//
+
+import SwiftUI
+
+@main
+struct ImageViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
